9d69c81ebc5d37fd3c8517a963acf633771bb2b0
TheMikal <131578548+TheMikal@users.noreply.github.com>
2023-10-25 00:44:18 -0400

    blog post routes

---
+++ controllers/api/blogPostRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const { BlogPost } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// create new blog post
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newBlogPost = await BlogPost.create({
+        ...req.body,
+        user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newBlogPost);
+    } catch (err) {
+        console.log(err)
+        res.status(400).json(err);
+    }
+});
+
+// edit existing blog post
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+        });
+
+        if (!blogPostData) {
+        res.status(404).json({ message: "Sorry chief that isn't real" });
+        return;
+        }
+
+        res.status(200).json(blogPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// delete existing blog post
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.destroy({
+        where: {
+            id: req.params.id,
+        },
+        });
+
+        if (!blogPostData) {
+        res.status(404).json({ message: "Yikes what was this supposed to be?" });
+        return;
+        }
+
+        res.status(200).json(blogPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
