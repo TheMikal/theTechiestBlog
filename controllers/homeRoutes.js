aa568016bcdf6078dad45e1a934f0cda0af06808
TheMikal <131578548+TheMikal@users.noreply.github.com>
2023-10-25 00:38:24 -0400

    server and homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,156 @@
+// Imports
+const router = require("express").Router();
+const { BlogPost, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findAll({
+        include: [
+            {
+            model: User,
+            attributes: ["name"],
+            },
+            {
+            model: Comment,
+            attributes: ["comment_body"],
+            },
+        ],
+        });
+
+        // Serialize data so template can read it
+        const blogPosts = blogPostData.map((blogPost) =>
+        blogPost.get({ plain: true })
+        );
+
+        res.render("homepage", {
+        blogPosts,
+        logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Route for single blog post 
+router.get("/blogPost/:id", withAuth, async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findByPk(req.params.id, {
+        include: [
+            {
+            model: User,
+            attributes: ["name"],
+            },
+            {
+            model: Comment,
+            include: [User],
+            },
+        ],
+        });
+
+        const blogPost = blogPostData.get({ plain: true });
+        console.log(blogPost);
+
+        res.render("blogPost", {
+        ...blogPost,
+        logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+        res.redirect("/login");
+    }
+});
+
+// authorized user goes to homepage
+router.get("/dashboard", withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ["password"] },
+        include: [
+            {
+            model: BlogPost,
+            include: [User],
+            },
+            {
+            model: Comment,
+            },
+        ],
+        });
+
+        const user = userData.get({ plain: true });
+        console.log(user)
+
+        res.render("dashboard", {
+        ...user,
+        logged_in: true,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/create", async (req, res) => {
+    try {
+        if (req.session.logged_in) {
+        res.render("create", {
+            logged_in: req.session.logged_in,
+            userId: req.session.user_id,
+        });
+        return;
+        } else {
+        res.redirect("/login");
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// edit an existing blog post
+router.get("/create/:id", async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findByPk(req.params.id, {
+        include: [
+            {
+            model: User,
+            attributes: ["name"],
+            },
+            {
+            model: Comment,
+            include: [User],
+            },
+        ],
+        });
+
+        const blogPost = blogPostData.get({ plain: true });
+        console.log(blogPost);
+
+        if (req.session.logged_in) {
+        res.render("edit", {
+            ...blogPost,
+            logged_in: req.session.logged_in,
+            userId: req.session.user_id,
+        });
+        return;
+        } else {
+        res.redirect("/login");
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.all("/login", (req, res) => {
+  // redirects if logged in
+    if (req.session.logged_in) {
+        res.redirect("/dashboard");
+        return;
+    }
+
+    res.render("login");
+});
+
+// Export
+module.exports = router;
